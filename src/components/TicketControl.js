2059f7c051b807c475b0f9f1cea9df5c2e416bf7
Laura Hope <ssteamlaura@gmail.com>
2023-06-27 08:27:29 -0700

    Add local/shared state

---
+++ src/components/TicketControl.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+
+class TicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+    this.handleClick = this.handleClick.bind(this); //new code here
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  }
+
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = null; // new code
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm />;
+      buttonText = "Return to Ticket List"; // new code
+    } else {
+      currentlyVisibleState = <TicketList />;
+      buttonText = "Add Ticket"; // new code
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button> { /* new code */ }
+      </React.Fragment>
+    );
+  }
+}
+
+export default TicketControl;
